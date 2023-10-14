5f075463550b28d3136f6694f91dd601ecb5dbae
Laureoby <laureoby@gmail.com>
2023-10-14 13:58:48 +0100

    Form Validation with React JS

--- src/app.js
+++ src/app.js
@@ -3,57 +3,29 @@ import './index.css'
 
 function App(){
     //state
-    const [poids, setPoids] = useState(0);
-    const [taille, setTaille] = useState(0);
-    const [imc, setImc] = useState(0);
-    const [msg, setMsg] = useState('');
-    const [imgSrc, setImgSrc] = useState('');
+    const [mail, setMail] = useState('')
+    const [msg, setMsg] = useState('')
 
     //Calcul imc
-    let CalculImc = (e) => {
-            setTaille(e.target.value)
-            let imc = (poids / (taille*taille*0.01))
-            setImc(imc.toFixed(1))
-            let imgSrc;
-
-            //Message en fonction du calcul de l'IMC et affichage image
-            if (imc <= 18.4){
-               setMsg('Vous êtes maigre')
-               setImgSrc(require('/home/laureoby/Bureau/ReactProjects/BMIProject/src/assets/search.png'))
-            }else if(imc >= 18.5 && imc <= 24.9){
-               setMsg('Votre corpulence est normale')
-               setImgSrc(require('/home/laureoby/Bureau/ReactProjects/BMIProject/src/assets/party.png'))
-            }else if(imc >= 25.0 && imc <= 39.9){
-               setMsg('Vous êtes en surpoids')
-               setImgSrc(require('/home/laureoby/Bureau/ReactProjects/BMIProject/src/assets/trouble.png'))
-            }else{
-               setMsg('Prenez des précautions, vous êtes obèse!!')
-               setImgSrc(require('/home/laureoby/Bureau/ReactProjects/BMIProject/src/assets/calendar.png'))
-            }
+    let Validation = (e) => {
+        setMail(e.target.value)
+        let emailReg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i);
+        emailReg.test(mail) ? setMsg(<p className="text-primary">Awesome!!! Your address email is correct!</p>) : setMsg(<p className="text-danger">Please enter a valid address email</p>)
     }
 
     return(
         <div className="app">
             <div className="container text-center mx-auto">
-                <h2 className="mt-4 pt-4">Calculateur IMC</h2>
+                <h2 className="mt-4 pt-4 mb-4 pb-4">Form Validation</h2>
                 <form>
                     <div>
-                        <label>Poids (en kg)</label>
-                        <input type="text" value={poids} onChange={(e) => setPoids(e.target.value)}/>
-                    </div>
-                    <div>
-                        <label>Taille (en cm)</label>
-                        <input type="text" value={taille} onChange={CalculImc}/>
+                        <label>Email Address</label>
+                        <input type="text" value={mail} onChange={Validation} placeholder="example@gmail.com"/>
                     </div>
                 </form>
 
                 <div className="mt-4">
-                    <h2>Votre IMC est {imc}</h2>
-                    <h6>{msg}</h6>
-                </div>
-
-                <div className="img-fluid">
-                    <img src={imgSrc} alt='' />
+                    <p>{msg}</p>
                 </div>
             </div>
         </div>
